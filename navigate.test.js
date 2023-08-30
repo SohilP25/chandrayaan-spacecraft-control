e395fc25569ebc8dfce9f54d49cc20bb08c3b7d1
Sohil Pathan <srpathan254@gmail.com>
2023-08-30 09:50:18 +0530

    Check for  different coordinates and all direction

--- navigate.test.js
+++ navigate.test.js
@@ -19,3 +19,38 @@ describe("Defination and type of spacecraft ", () => {
     expect(spacecraft.dir).toBeFalsy();
   });
 });
+
+/*
+    Test-2
+    ->Define spacecraft with different coordinates and all direction.
+    ->Test all initial coordinates and values.
+*/
+//Create a dummy spacecrafts that contain a all directions and diffrent coordinates.
+const dummySpacecraftN = new spacecraft(4,9,1,"N");
+const dummySpacecraftS = new spacecraft(2,5,1,"S");
+const dummySpacecraftE = new spacecraft(2,6,1,"E");
+const dummySpacecraftW = new spacecraft(0,22,1,"W");
+const dummySpacecraftU = new spacecraft(31,32,1,"U");
+const dummySpacecraftD = new spacecraft(0,13,61,"D");
+
+describe('All values are initialized properly', () => {
+    //Check for all directon.
+    test('Initialize with all direction', () => { 
+         expect(dummySpacecraftN.dir).toBe("N");
+         expect(dummySpacecraftS.dir).toBe("S");
+         expect(dummySpacecraftE.dir).toBe("E");
+         expect(dummySpacecraftW.dir).toBe("W");
+         expect(dummySpacecraftU.dir).toBe("U");
+         expect(dummySpacecraftD.dir).toBe("D");
+        })
+
+    //Check for different coordinates.
+    test('Intialize with differrnt positions', () => { 
+        expect(dummySpacecraftN.x).toBe(4);
+        expect(dummySpacecraftN.y).toBe(9);
+        expect(dummySpacecraftN.z).toBe(1);
+        expect(dummySpacecraftU.x).toBe(31);
+        expect(dummySpacecraftW.y).toBe(22);
+        expect(dummySpacecraftD.z).toBe(61);
+     })
+});
