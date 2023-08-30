a338cb1edba3fdec627e1876aa817ce23e64aa53
Sohil Pathan <srpathan254@gmail.com>
2023-08-30 10:48:13 +0530

    Add input from CLI adds interface and import package

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import package for I/O from CLI.
+const readline = require("readline");
+
+
+// Create a interface for I/O.
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
