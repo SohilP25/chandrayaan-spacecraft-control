ebab16e1e55808787a7d22f71c6eaf340d3a9124
Sohil Pathan <srpathan254@gmail.com>
2023-08-30 09:39:55 +0530

    Intialize spacecraft with all necessary values to navigate

---
+++ navigate.js
@@ -0,0 +1,17 @@
+// Initialize a class for spacecraft which can be any spacecraft(e.g.chandrayaan)
+class spacecraft {
+    /* 
+    The spacecraft navigates through the galaxy using galactic coordinates represented by (x, y, z).
+    coordinates (variable-x for east or west location, 
+                variable-y for north or south location,
+                variable-z for distance above or below the galactic plane).
+    variable-dir for initial facing direction.(N, S, E, W, U, D).
+    */
+    constructor(x, y, z, dir) {
+        // Create a new instance with coordinates and initial direction.
+      this.x = parseInt(x);
+      this.y = parseInt(y);
+      this.z = parseInt(z);
+      this.dir = dir;
+    }
+}
---
+++ navigate.test.js
@@ -0,0 +1,21 @@
+// import necessary modules and packages
+const spacecraft = require("./navigate");
+
+/*
+    Test-1 
+    ->Define type of spacecraft and it contain all necessary values to navigate.
+    ->Define a spacecraft with initial values and check for undefined(falsy) values.
+*/
+describe("Defination and type of spacecraft ", () => {
+
+  test("spacecraft is a function", () => {
+    expect(typeof spacecraft).toBe("object");
+  });
+
+  test("All spacecraft values are undefined at first", () => {
+    expect(spacecraft.x).toBeFalsy();
+    expect(spacecraft.y).toBeFalsy();
+    expect(spacecraft.z).toBeFalsy();
+    expect(spacecraft.dir).toBeFalsy();
+  });
+});
